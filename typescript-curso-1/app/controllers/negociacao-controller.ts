e6b72367ff47c5a32663f3deb392bb35bbcca383
luzialeticia <leticia.luzia@thoughtworks.com>
2021-09-28 20:15:43 -0300

    Isolando método para criar negociação

--- typescript-curso-1/app/controllers/negociacao-controller.ts
+++ typescript-curso-1/app/controllers/negociacao-controller.ts
@@ -11,13 +11,17 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
 
-    adiciona() {
+    criaNegociacao() {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
 
-        const negociacao = new Negociacao(date, quantidade, valor);
+        return new Negociacao(date, quantidade, valor);
+    }
+
+    adiciona() {
+        const negociacao = this.criaNegociacao();
 
         console.log(negociacao);
     }
